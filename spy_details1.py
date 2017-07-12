883dea11b4f4c84a5733fce9c8a93db09155b1b5
rahulshivu <rahulkoundal1996@gmail.com>
2017-07-12 15:53:16 +0530

    commenting in the spy_details.py

--- spy_details1.py
+++ spy_details1.py
@@ -1,4 +1,9 @@
+# import datetime
+
 from datetime import datetime
+
+# class spy
+
 
 class Spy:
 
@@ -12,18 +17,20 @@ class Spy:
         self.current_status_message = None
 
 
-class ChatMessage:
+# class chatmessage
 
+class ChatMessage:
+     #constructor of the class
     def __init__(self,message,sent_by_me):
         self.message = message
         self.time = datetime.now()
         self.sent_by_me = sent_by_me
-
+# value is given
 spy = Spy('bond', 'Mr.', 24, 4.7)
 
 friend_one = Spy('Raja', 'Mr.', 4.9, 27)
 friend_two = Spy('Mata Hari', 'Ms.', 4.39, 21)
 friend_three = Spy('No', 'Dr.', 4.95, 37)
 
-
+#list of friends
 friends = [friend_one, friend_two, friend_three]
