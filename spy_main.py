35f5dbf95f6bfe35ed2a45ee7a70a70fd7eb79f5
rahulshivu <rahulkoundal1996@gmail.com>
2017-07-25 11:50:21 +0530

    first commit

--- spy_main.py
+++ spy_main.py
@@ -21,50 +21,50 @@ existing = raw_input(question)
 # add_status() function to add status
 # designation of the function
 
-
 def add_status():
 
     updated_status_message = None
 
     if spy.current_status_message != None:
 
-        print("Your current status message is %s \n' % (spy.current_status_message)")
+        print 'Your current status message is %s \n' % (spy.current_status_message)
     else:
-        print("You don\'t have any status message currently \n")
+        print 'You don\'t have any status message currently \n'
 
-        default = raw_input("Do you want to select from the older status (y/n)? ")
+    default = raw_input("Do you want to select from the older status (y/n)? ")
 
-        if default.lower() == "n":
-         new_status_message = raw_input("What status message do you want to set? ")
+    if default.upper() == "N":
+        new_status_message = raw_input("What status message do you want to set? ")
 
 
         if len(new_status_message) > 0:
             STATUS_MESSAGES.append(new_status_message)
             updated_status_message = new_status_message
 
-        elif default.lower() == 'y':
+    elif default.upper() == 'Y':
 
-         item_position = 1
+        item_position = 1
 
-         for message in STATUS_MESSAGES:
+        for message in STATUS_MESSAGES:
             print '%d. %s' % (item_position, message)
             item_position = item_position + 1
 
         message_selection = int(raw_input("\nChoose from the above messages "))
 
-# len() calculate the length of the variable
-    if len(STATUS_MESSAGES) >= message_selection:
+
+        if len(STATUS_MESSAGES) >= message_selection:
             updated_status_message = STATUS_MESSAGES[message_selection - 1]
 
     else:
         print 'The option you chose is not valid! Press either y or n.'
 
     if updated_status_message:
-        print("Your updated status message is: %s % (updated_status_message)")
+        print 'Your updated status message is: %s' % (updated_status_message)
     else:
-        print("You current don't have any  status update")
+        print 'You current don\'t have a status update'
 
     return updated_status_message
+
 
 # add_friend function to add the friend
 
